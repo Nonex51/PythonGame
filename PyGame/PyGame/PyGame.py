85934d51daae6e5a4de94dfcdd4e9eed974960eb
guillaumesibot <guillaumesibot@gmail.com>
2019-01-03 14:41:41 +0100

    Fonction game over and restart the game it's ok

--- PyGame/PyGame/PyGame.py
+++ PyGame/PyGame/PyGame.py
@@ -1,6 +1,7 @@
 import pygame
+import time
 
-blue = (0,0,255)
+blue = (113,177,227)
 white = (255,255,255)
 
 pygame.init()
@@ -12,9 +13,48 @@ ballonH = 66
 
 surface = pygame.display.set_mode((surfaceW, surfaceH))
 pygame.display.set_caption("Fly Balloon")
+clock = pygame.time.Clock()
 
 img = pygame.image.load('Ballon01.png')
 
+def PlayAgain():
+    for event in pygame.event.get ([pygame.KEYDOWN, pygame.KEYUP,pygame.QUIT]):
+        if event.type == pygame.QUIT :
+            pygame.QUIT()
+            quit()
+        elif event.type == pygame.KEYUP :
+            continue
+        return event.key
+    return None
+
+def creaTextObj(text, Police):
+    textSurface = Police.render(text,True,white)
+    return textSurface, textSurface.get_rect()
+
+def message (text):
+    BText = pygame.font.Font('BradBunR.ttf', 150)
+    SText = pygame.font.Font('BradBunR.ttf', 20)
+
+    BTextSurf, BTextRect = creaTextObj(text, BText)
+    BTextRect.center = surfaceW/2, ((surfaceH/2)-50)
+    surface.blit(BTextSurf,BTextRect)
+    
+    STextSurf, STextRect = creaTextObj("Press a touch to continu",SText)
+    STextRect.center = surfaceW/2, ((surfaceH/2)+50)
+    surface.blit(STextSurf,STextRect)
+
+    pygame.display.update()
+    time.sleep(2)
+
+    while PlayAgain () == None :
+        clock.tick()
+
+    principal()
+
+
+
+def gameOver():
+    message("Boom")
 
 def ballon (x,y,image):
     surface.blit(image, (x,y))
@@ -23,7 +63,8 @@ def ballon (x,y,image):
 def principal():
     x = 150
     y = 200
-  
+    y_mouvement = 0
+
     #create a variable to now if the game need to stop
     game_over = False
     while not game_over :
@@ -31,8 +72,20 @@ def principal():
             if event.type == pygame.QUIT:
                 game_over = True
 
+            if event.type == pygame.KEYDOWN :
+                if event.key == pygame.K_UP :
+                     y_mouvement = -1
+            if event.type == pygame.KEYUP :
+                y_mouvement = 1
+
+        y+= y_mouvement
+
         surface.fill(blue)
         ballon(x,y,img)
+
+        if y >surfaceH -40 or y < -10 :
+            gameOver()
+
         pygame.display.update()
 
 
