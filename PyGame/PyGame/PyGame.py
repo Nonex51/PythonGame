8e997d4abb89eaeaf20e5b4f3774740bf55f5e7c
guillaumesibot <guillaumesibot@gmail.com>
2019-01-03 19:12:57 +0100

    Sqlite create a DB.file but error line 106

--- PyGame/PyGame/PyGame.py
+++ PyGame/PyGame/PyGame.py
@@ -1,11 +1,30 @@
 import pygame
 import time
 from random import *
+import sqlite3
 
 blue = (113,177,227)
 white = (255,255,255)
 
 pygame.init()
+
+Donnees = "/Users/Guillaume/Documents/PythonGame/PyGame/Donnees.sq3"
+conn = sqlite3.connect(Donnees)
+cur = conn.cursor()
+
+
+"""
+#This code it's to create the file to save the Score
+#Donnees = "/Users/Guillaume/Documents/PythonGame/PyGame/Donnees.sq3"
+#Donnees = "/Users/Guillaume/Desktop/Donnees.sq3"
+conn = sqlite3.connect(Donnees)
+cur = conn.cursor()
+cur.execute("create table membres (score integer)")
+cur.execute("insert into membres (score) values (0)")
+conn.commit()
+cur.execute("insert into membres (score) values (0)")
+conn.commit()
+"""
 
 surfaceW = 800
 surfaceH = 500
@@ -25,7 +44,12 @@ img_cloud_2 =pygame.image.load('NuageBas.png')
 def score(compte):
     police = pygame.font.Font('BradBunR.ttf',16)
     text = police.render("Score: " + str(compte), True, white)
-    surface.blit(text, [10,0])                                  #position of score
+    surface.blit(text, [10,0])             #position of score
+
+def Hscore(compte):
+    police = pygame.font.Font('BradBunR.ttf',16)
+    text = police.render("High.Score: " + str(compte), True, white)
+    surface.blit(text, [700,0])         
 
 def clouds(x_cloud, y_cloud, espace):
     surface.blit(img_cloud_1,(x_cloud,y_cloud))
@@ -46,7 +70,7 @@ def creaTextObj(text, Police):
     textSurface = Police.render(text,True,white)            #define an object for the text 
     return textSurface, textSurface.get_rect()
 
-def message (text):
+def messageScreen (text):
     BText = pygame.font.Font('BradBunR.ttf', 150)
     SText = pygame.font.Font('BradBunR.ttf', 20)
 
@@ -66,8 +90,26 @@ def message (text):
 
     principal()
 
-def gameOver():
-    message("Boom")                       #display an object message
+def gameOver(score_actuel):
+    a = list(str(score_actuel))     #create a list with a char
+    Donnees = "/Users/Guillaume/Documents/PythonGame/PyGame/Donnees.sq3"
+    conn =sqlite3.connect(Donnees)
+    cur = conn.cursor()
+    cur.execute("select * from membres")
+    dataliste = list(cur)
+                                    #save the best score in the DB file
+    hscore=[]
+    for i in range(0,len(dataliste)):
+        hscore += dataliste[i]
+
+    if (int(hscore[-1])< score_actuel):
+        cur.execute("insert into membres(score) values(?)", a)
+        conn.commit()
+        cur.close()
+        conn.close()
+
+    messageScreen("Boom")                       #display an object message
+
 
 def ballon (x,y,image):
     surface.blit(image, (x,y))
@@ -80,7 +122,7 @@ def principal():                          #my principal function main()
 
     x_cloud = surfaceW
     y_cloud = randint(-300,20)                #random value
-    espace = ballonH*3                        #height of the space betwwen the clouds
+    espace = ballonH*3                        #height of the space between the clouds
     cloud_speed = 2
 
     score_actuel = 0
@@ -93,8 +135,10 @@ def principal():                          #my principal function main()
                                                 #detect if you press an input
             if event.type == pygame.KEYDOWN :
                 if event.key == pygame.K_UP :
+                #if event.key == pygame.K_SPACE :
                      y_mouvement = - ballon_speed
             if event.type == pygame.KEYUP :
+            #if event.type == pygame.K_SPACE :
                 y_mouvement = ballon_speed
 
         y+= y_mouvement
@@ -106,35 +150,43 @@ def principal():                          #my principal function main()
 
         score(score_actuel)
 
+        cur.execute("select * from membres")    #take all in the data list and put the result inside the cursor (cur)
+        dataliste = list(cur)                       # make a list with the data
+        print(cur)
+        print(dataliste)
+
+        hscore = []
+        for i in range (0,len(dataliste)):
+            hscore +=dataliste[i]                    # format the data in list from table
+        print(hscore)
+
+        Hscore(hscore[-1])
+
+
         x_cloud -= cloud_speed              #position of the cloud at each loop/frame
 
         if y >surfaceH -40 or y < -10 :     #when you touch the border of the windows
-            gameOver()
+            gameOver(score_actuel)
 
                                          #increase the difficulty in function of your score
-        if 3 <= score_actuel < 5 :
-            cloud_speed = 5
-            ballon_speed = 2
-            espace = ballonH *2.8
-        if 5 <= score_actuel < 7 :
-            cloud_speed = 7
-            ballon_speed = 4
-            espace = ballonH *2.7
-        if 7 <= score_actuel < 10 :
-            cloud_speed = 9
-            ballon_speed = 6
-            espace = ballonH *2.5
+        if 1 <= score_actuel < 100 :
+            cloud_speed = 2 + 0.5  * score_actuel
+            ballon_speed = 2 + 0.1 * score_actuel
+            espace = ballonH * 2.8 - 0.1*score_actuel
+       
 
+       
+  
                                          #define when you touch the cloud with the ballon, you can change the difficulty here
         if x + ballonW > x_cloud +40:
-            if y < y_cloud + cloudH -50:
+            if y < y_cloud + cloudH -80:               #Space tolerate inside the cloud up and down
                 if x - ballonW < x_cloud + cloudW -20 :
-                    gameOver()
+                    gameOver(score_actuel)
 
         if x +ballonW > x_cloud +40:
-            if y + ballonH > y_cloud + cloudH + espace + 50:
+            if y + ballonH > y_cloud + cloudH + espace + 80: #Space tolerate on the side of the cloud 
                 if x + ballonW < x_cloud +cloudW -20 :
-                    gameOver()
+                    gameOver(score_actuel)
 
 
         if x_cloud < (-1*cloudW) :              #give a new position for a new cloud
@@ -152,9 +204,7 @@ def principal():                          #my principal function main()
 
         pygame.display.update()
        
-
-
-     
+    
 principal()
     #os._exit()
 pygame.quit()
